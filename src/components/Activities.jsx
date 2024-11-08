f742cdad9885c065b92c12627e4fdc4d49f10e9e
Jlorduyz <jorgelorduyeli4@outlook.com>
2024-11-08 15:28:26 -0500

    bug

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -2,9 +2,9 @@ import { useState } from "react"
 
 function Activities() {
     const [panel, setPanel] = useState(false)
-    return (<div>
+    return (<div className="">
         <button onClick={() => setPanel(!panel)} className="flex justify-center w-full text-2xl text-white">{panel ? "🔽Activities🔽" : "🔼Activities🔼"}</button>
-        {panel ? <div className="w-96 h-20 flex justify-center items-center rounded-b-md bg-indigo-500"><p className="text-2xl text-white font-extrabold">Under construction</p></div> : ""}
+        {panel ? <div className="w-full h-auto p-6 flex justify-center items-center rounded-b-md bg-indigo-500"><p className="text-2xl text-white font-extrabold">Under construction</p></div> : ""}
     </div>)
 }
 
